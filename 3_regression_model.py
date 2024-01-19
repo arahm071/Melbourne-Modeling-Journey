8678512f657a11eadeec4f79ad158f954fdc90ba
Asif Rahman <arahm071@uottawa.ca>
2024-01-18 22:09:14 -0500

    Refactor regression model script for enhanced clarity and readability
    - Improve variable naming for greater clarity and consistency throughout the script.
    - Refactor the creation and concatenation of dummy variables for better code organization.
    - Adjust code formatting and spacing for improved readability and adherence to Python best practices.
    - Add detailed comments to explain the purpose and functionality of code blocks and individual operations.
    - Ensure consistency in style and structure to enhance maintainability of the regression model script.

--- 3_regression_model.py
+++ 3_regression_model.py
@@ -1,43 +1,36 @@
-#Import stuff
+# Importing necessary libraries
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import statsmodels.api as sm
 
-# Load dataset containing transformed Melbourne housing data
+# Load the transformed Melbourne housing dataset
 melb_data = pd.read_csv('2_transformed_melb_data.csv')
 
+# Creating dummy variables for categorical columns
+dummies_Suburb = pd.get_dummies(melb_data['Suburb'], drop_first=True, dtype=int)
+dummies_Regionname = pd.get_dummies(melb_data['Regionname'], drop_first=True, dtype=int)
+dummies_Type = pd.get_dummies(melb_data['Type'], drop_first=True, dtype=int)
+dummies_Method = pd.get_dummies(melb_data['Method'], drop_first=True, dtype=int)
 
-#Dummy Catergorical Columns
-dummy_Suburb = pd.get_dummies(melb_data['Suburb'], drop_first=True, dtype=int)
-dummy_Regionname = pd.get_dummies(melb_data['Regionname'], drop_first=True, dtype=int)
-dummy_Type = pd.get_dummies(melb_data['Type'], drop_first=True, dtype=int)
-dummy_Method = pd.get_dummies(melb_data['Method'], drop_first=True, dtype=int)
+# Selecting the top N suburbs for creating grouped suburb categories
+num_top_suburbs = 50
+top_suburbs = melb_data['Suburb'].value_counts().nlargest(num_top_suburbs).index
+melb_data['Grouped_Suburb'] = melb_data['Suburb'].apply(lambda x: x if x in top_suburbs else 'Other')
+dummies_Grouped_Suburb = pd.get_dummies(melb_data['Grouped_Suburb'], drop_first=True, dtype=int)
 
-#Dummy Suburbs Column
-N = 50
+# Concatenating the dummy variables with the main DataFrame
+melb_data = pd.concat([melb_data, dummies_Regionname, dummies_Type, dummies_Method, dummies_Grouped_Suburb], axis=1)
 
-top_suburbs = melb_data['Suburb'].value_counts().nlargest(N).index
+# Defining the columns to exclude from the regression model
+excluded_columns = ['Address', 'Suburb', 'Regionname', 'Type', 'Price_log', 'Method', 'Date', 'Propertycount', 'Grouped_Suburb']
 
-melb_data['Suburb_Grouped'] = melb_data['Suburb'].apply(lambda x: x if x in top_suburbs else 'Other')
-
-dummy_Suburb_Grouped = pd.get_dummies(melb_data['Suburb_Grouped'], drop_first=True, dtype=int)
-
-
-#Join Dummies columns to main df
-melb_data = pd.concat([melb_data, dummy_Regionname, dummy_Type, dummy_Method], axis=1)
-
-#skip columns in list
-skip_column = ['Address', 'Suburb', 'Regionname', 'Type', 'Price_log', 'Method', 'Date', 'Propertycount', 'Suburb_Grouped']
-
-#Regression Model
-X = melb_data[[x for x in melb_data.columns if x not in skip_column]]
-
+# Preparing the features (X) and target variable (y) for the regression model
+X = melb_data[[x for x in melb_data.columns if x not in excluded_columns]]
 X = sm.add_constant(X)
-
 y = melb_data['Price_log']
 
-model = sm.OLS(y,X).fit()
-
+# Fitting the OLS regression model and printing the summary
+model = sm.OLS(y, X).fit()
 print(model.summary())
