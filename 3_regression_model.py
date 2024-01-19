2e18d27b73b88b492b7863e8701f85da4ae8b6fb
Asif Rahman <arahm071@uottawa.ca>
2024-01-18 23:23:33 -0500

    Remove dummies_Grouped_Suburb from melb_data concatenation
    - Removed dummies_Grouped_Suburb, created from dummies_Suburb with 100+ variables.
    - Aimed to reduce variables to top N, testing impact on R-squared value.
    - Found minimal R-squared difference (0.8 with total suburb dummies vs. 0.7 without).
    - Concluded that region-based classification is more effective than numerous suburb variables.
    classifier is more efficient.
    - 'Region' groups suburbs more cohesively (less variables used), unlike individual suburbs which are too spread out.

--- 3_regression_model.py
+++ 3_regression_model.py
@@ -9,22 +9,22 @@ import statsmodels.api as sm
 melb_data = pd.read_csv('2_transformed_melb_data.csv')
 
 # Creating dummy variables for categorical columns
-dummies_Suburb = pd.get_dummies(melb_data['Suburb'], drop_first=True, dtype=int)
+# ! dummies_Suburb = pd.get_dummies(melb_data['Suburb'], drop_first=True, dtype=int)
 dummies_Regionname = pd.get_dummies(melb_data['Regionname'], drop_first=True, dtype=int)
 dummies_Type = pd.get_dummies(melb_data['Type'], drop_first=True, dtype=int)
 dummies_Method = pd.get_dummies(melb_data['Method'], drop_first=True, dtype=int)
 
-# Selecting the top N suburbs for creating grouped suburb categories
+'''# Selecting the top N suburbs for creating grouped suburb categories
 num_top_suburbs = 50
 top_suburbs = melb_data['Suburb'].value_counts().nlargest(num_top_suburbs).index
 melb_data['Grouped_Suburb'] = melb_data['Suburb'].apply(lambda x: x if x in top_suburbs else 'Other')
-dummies_Grouped_Suburb = pd.get_dummies(melb_data['Grouped_Suburb'], drop_first=True, dtype=int)
+dummies_Grouped_Suburb = pd.get_dummies(melb_data['Grouped_Suburb'], drop_first=True, dtype=int)'''
 
 # Concatenating the dummy variables with the main DataFrame
-melb_data = pd.concat([melb_data, dummies_Regionname, dummies_Type, dummies_Method, dummies_Grouped_Suburb], axis=1)
+melb_data = pd.concat([melb_data, dummies_Regionname, dummies_Type, dummies_Method], axis=1)
 
 # Defining the columns to exclude from the regression model
-excluded_columns = ['Address', 'Suburb', 'Regionname', 'Type', 'Price_log', 'Method', 'Date', 'Propertycount', 'Grouped_Suburb']
+excluded_columns = ['Address', 'Suburb', 'Regionname', 'Type', 'Price_log', 'Method', 'Date', 'Propertycount']
 
 # Preparing the features (X) and target variable (y) for the regression model
 X = melb_data[[x for x in melb_data.columns if x not in excluded_columns]]
