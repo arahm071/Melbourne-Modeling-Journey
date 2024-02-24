1175b9d1b81fbd33f3df992b975541d0a00a1d22
Asif Rahman <arahm071@uottawa.ca>
2024-02-23 22:57:56 -0500

    Enhancements and Optimizations in Melbourne Data Cleaning Script
    - Introduced utility imports for advanced data visualization and manipulation.
    - Implemented a new directory structure for better module management and data file access.
    - Added `plot_utils` for more sophisticated visualization capabilities.
    - Enhanced data loading mechanism to accommodate date parsing for accurate datetime representations.
    - Introduced new data manipulation techniques:
      - Extraction of year and month from the date for deeper analysis.
    - Developed a comprehensive strategy for missing data visualization and imputation, including the use of `missingno` for graphical missing data analysis.
    - Extended data preparation with new binary indicators and median value imputations for critical columns like `Bathroom` and `Car`.
    - Implemented a custom function `fill_councilarea` for intelligent missing value imputation based on similar property attributes.
    - Streamlined the dataset by removing unnecessary columns and reordering for enhanced readability and analysis.
    - Conducted a detailed post-cleaning data inspection, including summarization and missing value checks.
    - Addressed outlier detection and handling in key quantitative columns, employing statistical methods for robust data cleaning.
    
    This update introduces significant improvements in data cleaning precision, handling of missing values, outlier management, and dataset organization, laying a solid foundation for advanced data analysis.

--- 01_Data_Cleaning/1_clean_melb_data.py
+++ 01_Data_Cleaning/1_clean_melb_data.py
@@ -1,34 +1,67 @@
 # Standard library imports
-import os              # Provides a way of using operating system dependent functionality
+import sys # Provides a way of using operating system dependent functionality
+import os  # For interacting with the operating system
 from datetime import datetime  # For handling date and time data
 
 # Third-party imports
-import pandas as pd             # Popular data manipulation and analysis library for Python
-import numpy as np              # Fundamental package for scientific computing in Python
-import matplotlib.pyplot as plt # Plotting library for creating static, animated, and interactive visualizations
-import seaborn as sns           # Data visualization library based on matplotlib
+import pandas as pd  # For data manipulation and analysis
+import numpy as np  # For numerical computations
+import matplotlib.pyplot as plt  # For creating visualizations
+import seaborn as sns  # For high-level data visualization
+import missingno as msno  # For visualizing missing data
 
-# Define a lambda function for date parsing in the dataset
-# This is used to convert date strings into datetime objects
-dateparse = lambda x: datetime.strptime(x, '%d/%m/%Y')
+# Define the absolute path of the parent directory of the script's grandparent directory
+# This is useful for module importation from a different directory structure
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, parent_dir)  # Add it to the sys.path for module resolution
 
-# Get the absolute path to the directory of the current script
+# Local application imports
+from utils import plot_utils  # Importing visualization utilities
+
+# * File Importation
+# Determine the absolute path to the directory containing the current script
 script_dir = os.path.abspath(os.path.dirname(__file__))
 
-# Build the absolute path to the data file
+# Construct the path to the data file
 data_path = os.path.join(script_dir, '..', '00_Raw_Data', '0_melb_data.csv')
 
-# Load the dataset with custom date parsing for the 'Date' column
-# and immediately convert datetime objects to date format
-melb_data = pd.read_csv(data_path, parse_dates=['Date'], date_parser=dateparse)
-melb_data['Date'] = melb_data['Date'].dt.date
+# Load the dataset, parsing dates to ensure proper datetime format
+melb_data = pd.read_csv(data_path, parse_dates=['Date'], dayfirst=True)
 
-# Remove duplicate entries from the dataset to ensure data integrity
+# Data manipulation: extracting year and month from the date for further analysis
+melb_data['Year'] = melb_data['Date'].dt.year
+melb_data['Month'] = melb_data['Date'].dt.month
+melb_data['Date'] = melb_data['Date'].dt.date  # Simplify 'Date' to date format
+
+# Remove duplicate entries to maintain data integrity
 melb_data.drop_duplicates(inplace=True)
 
-# Fill missing values in the 'Car' column with 0
-# Assuming that missing values imply no car space available
-melb_data.fillna(value={'Car': 0}, inplace=True)
+# * Pre-Cleaned Data Inspection
+# Display summary statistics for the dataset
+print(melb_data.describe())
+
+# Identify missing values across columns
+print(melb_data.isna().sum())
+
+# Visualize missing data to guide cleaning and imputation strategies
+msno.matrix(melb_data)
+plt.show()
+
+# * Data Preparation for Model Fitting
+# Create binary indicators for specific conditions to enhance model interpretation
+melb_data['Bathroom_was_0'] = np.where(melb_data['Bathroom'] == 0, True, False)
+melb_data['Car_was_missing'] = melb_data['Car'].isna()
+melb_data['Landsize_Indicator'] = np.where(melb_data['Landsize'] == 0, 1, 0)
+
+# TODO: Create indicator variable for 'BuildingArea'
+
+# Replace 0 bathroom counts with the median number of bathrooms.
+melb_data['Bathroom'] = np.where(melb_data['Bathroom'] == 0, melb_data['Bathroom'].median(), melb_data['Bathroom'])
+
+# Fill missing values in 'Car' column with the median number of cars.
+melb_data.fillna(value={'Car': melb_data['Car'].median()}, inplace=True)
+
+# TODO: Using Random Forest for Predictive Imputation for BuildingArea
 
 # Define a function to determine the effective number of bedrooms
 # based on 'Rooms' and 'Bedroom2' columns
@@ -58,31 +91,74 @@ def effective_bedrooms(row):
 # Apply the effective_bedrooms function to the dataset
 melb_data['NewBed'] = melb_data.apply(effective_bedrooms, axis=1)
 
-# Ensure that every property is listed with at least one bathroom
-melb_data['Bathroom'] = np.where(melb_data['Bathroom'] == 0, 1, melb_data['Bathroom'])
+def fill_councilarea(row):
+    """
+    Fill missing 'CouncilArea' based on properties in the same suburb.
 
-# Drop columns that are not needed for further analysis or have many missing values
-melb_data.drop(columns=['SellerG', 'Postcode', 'CouncilArea', 
-                        'Lattitude', 'Longtitude', 'BuildingArea', 
-                        'YearBuilt', 'Rooms', 'Bedroom2'], inplace=True)
+    This function matches properties by 'Suburb', 'Postcode', 'Regionname', 
+    and 'Propertycount' to fill missing 'CouncilArea' values. If a matching 
+    property with a non-null 'CouncilArea' is found, its 'CouncilArea' value 
+    is used.
 
+    Parameters:
+    row (pd.Series): A row from the DataFrame.
 
-# Reorder columns for a more organized DataFrame
-melb_data = melb_data[['Address', 'Suburb', 'Regionname', 'Type', 'Price', 'Method', 
-                       'Date', 'Distance', 'NewBed', 'Bathroom', 'Car', 
-                       'Landsize', 'Propertycount']]
+    Returns:
+    str: The filled or original 'CouncilArea' value for the row.
+    """
+    if pd.isnull(row['CouncilArea']):
+        match = melb_data[(row['Suburb'] == melb_data['Suburb']) & 
+                          (row['Postcode'] == melb_data['Postcode']) & 
+                          (row['Regionname'] == melb_data['Regionname']) & 
+                          (row['Propertycount'] == melb_data['Propertycount']) & 
+                          pd.notnull(melb_data['CouncilArea'])]
+        if not match.empty:
+            return match.iloc[0]['CouncilArea']
+    return row['CouncilArea']
 
+melb_data['CouncilArea'] = melb_data.apply(fill_councilarea, axis=1)
+
+# Simplify the dataset by dropping columns not needed for further analysis
+melb_data.drop(columns=['Rooms', 'Bedroom2', 'YearBuilt', 'Lattitude', 'Longtitude', 'Propertycount'], inplace=True)
+
+# Reorder columns for readability and analysis purposes
+melb_data = melb_data[[
+    'Address', 'Postcode', 'Suburb', 'Regionname', 'CouncilArea', 'Type', 
+    'Price', 'SellerG', 'Method', 'Date', 'Year', 'Month', 'NewBed', 
+    'Bathroom', 'Bathroom_was_0', 'Car', 'Car_was_missing', 'Distance',
+    'Landsize', 'Landsize_Indicator', 'BuildingArea'
+]]
+
+# * Post-Cleaning Data Inspection
+# Summarize the cleaned dataset to verify changes
+print(melb_data.describe())
+
+# Check for remaining missing values post-cleaning
+print(melb_data.isna().sum())
+
+# Visualize the cleaned data to ensure readiness for analysis
+msno.matrix(melb_data)
+plt.show()
+print(melb_data.shape[0])
+
+# Outlier handling: Identify and mitigate outliers in key quantitative columns
+quan_columns = ['Price', 'NewBed', 'Bathroom', 'Car', 'Distance', 'Landsize']
+plot_utils.plot_box(data=melb_data, column_list=quan_columns, rows=2, cols=3) # Utilize utility for boxplot visualization
+    
+# Identify and handle outliers for 'Landsize' column
+q1, q3 = melb_data['Landsize'].quantile([0.25, 0.75])
+IQR = q3 - q1
+upper, lower = q3 + (1.5 * IQR), q1 - (1.5 * IQR)
+melb_data = melb_data[(melb_data['Landsize'] >= lower) & (melb_data['Landsize'] <= upper)].copy()
+
+# Re-visualize post-outlier adjustment to verify correction
+plot_utils.plot_box(data=melb_data, column_list=quan_columns, rows=2, cols=3)
+plt.show()
+print(melb_data.shape[0])
+
+# * Export Data
 # Construct the full file path
 output_file_path = os.path.join(script_dir, '1_cleaned_melb_data.csv')
 
 # Export the cleaned and processed data to a new CSV file
-melb_data.to_csv(output_file_path, index=False)
-
-# Uncomment the following lines to explore the dataset
-# print(melb_data.describe())
-# print(melb_data[melb_data['Landsize'] == 0]['Type'].value_counts())
-
-# Identify and sort duplicate listings based on specific columns
-columns_names = ['Address', 'Suburb', 'Propertycount']
-duplicates = melb_data.duplicated(subset=columns_names, keep=False)
-dup_melb_data = melb_data[duplicates].sort_values(by=['Address', 'Suburb', 'Date'])
+melb_data.to_csv(output_file_path, index=False)  # Export without the index
